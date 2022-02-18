9e1c7fd0703fc203aee23cf9db5e2229fd57704a
swlee <zkdlfrlwl3@naver.com>
2022-02-18 11:50:58 +0900

    feat: add prom

--- app/api.py
+++ app/api.py
@@ -3,6 +3,7 @@ import numpy as np
 from fastapi import FastAPI, Response
 from joblib import load
 from .schemas import Wine, Rating, feature_names
+from .monitoring import instrumentator
 
 ROOT_DIR = Path(__file__).parent.parent
 
@@ -11,6 +12,7 @@ app = FastAPI()
 scaler = load(ROOT_DIR / "artifacts/scaler.joblib")
 model = load(ROOT_DIR / "artifacts/model.joblib")
 
+instrumentator.instrument(app).expose(app, include_in_schema=False, should_gzip=True)
 
 @app.get("/")
 def root():
